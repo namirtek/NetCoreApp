a9c9a406179168e3efd133a67e2005a71752cf40
namirtek <119343925+namirtek@users.noreply.github.com>
2023-12-06 19:10:17 +0000

    End of section 3

--- Program.cs
+++ Program.cs
@@ -10,8 +10,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+//1- enabling CORS
+builder.Services.AddCors();
  
 var app = builder.Build();
+
+//2-enabling CORS (observer)
+app.UseCors(builder => builder.AllowAnyMethod().AllowAnyHeader().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
